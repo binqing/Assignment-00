38a87b3f1b80aafcc0b345c437270658fa4c926d
Binqing Zheng <bqz2101@gmail.com>
2021-01-01 19:12:45 -0500

    Add files via upload

---
+++ 00-greetFriend/greetFriend.js
@@ -0,0 +1,12 @@
+
+function greetFriend(name) {
+  // Insert code here;
+  var s = "Hello, " + name +"!";
+  return s;
+}
+
+console.log(greetFriend("Adam"));
+
+// Do not edit this line;
+module.exports = greetFriend;
+
